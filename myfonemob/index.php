555bf8abc4cdf9cfcabdc37cb71123fa5079c2b2
nollyvenon <gooption@yahoo.com>
2016-06-07 22:26:50 +0100

    3wc app
    3wc app

---
+++ myfonemob/index.php
@@ -0,0 +1,7 @@
+<head>
+    <link rel="apple-touch-icon" href="images/logo.png" />
+	<link rel="shortcut icon" href="images/logo.png">
+<link rel="icon" type="image/png" href="images/logo.png">
+<title> MyFoneTV </title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php header("Location:http://myfonetv.com/index.php");?>
